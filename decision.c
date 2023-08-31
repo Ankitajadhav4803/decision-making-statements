308587ea4725cda9b7fb408510294cc4f6bce440
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-08-31 19:13:10 +0530

    Add files via upload

---
+++ decision.c
@@ -0,0 +1,12 @@
+/* c program to accept an integer and check if it is even or odd */
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter any number=");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("given number is even.");
+	else
+	printf("given number is odd.");	
+}
