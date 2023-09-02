056ccb5e7acb2530e658b89aa25f518b8201afc6
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-02 18:48:13 +0530

    Add files via upload

---
+++ decision2.c
@@ -0,0 +1,12 @@
+/* c program to accept a number and check if it is divisible by 5 and 7 */
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter any number=");
+	scanf("%d",&num);
+	if(num%5==0 && num%7==0)
+	printf("given number is divisible by 5 and 7.");
+	else
+	printf("given number is not divisible by 5 and 7.");
+}
