391094218efd6eba3b3a777e6ea66c5a3587f337
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-15 19:21:08 +0530

    Add files via upload

---
+++ decision14.c
@@ -0,0 +1,25 @@
+/* c program , which accepts two integers and an operator as a character (+ - * /), performs the corresponding operation and displays the result */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	char ch;
+	printf("enter two integers=");
+	scanf("%d%d",&a,&b);
+	printf("\n enter choice=+,-,*,/ \n");
+	ch=getch();
+	switch(ch)
+	{
+	case '+':printf("addition=%d",a+b);
+			break;
+	case '-':printf("substraction=%d",a-b);
+			break;
+	case '*':printf("multiplication=%d",a*b);
+			break;
+	case '/':printf("division=%d",a/b);
+			break;
+	default:printf("invalid choice.");
+		   break;
+	}
+}
