487f3895db95ac37f24dd62b86fa602941e6a31b
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-17 16:05:55 +0530

    Add files via upload

---
+++ decision16.c
@@ -0,0 +1,38 @@
+/* c program to accept months in number from the user and display it in words */
+#include<stdio.h>
+void main()
+{
+	int m;
+	printf("enter month in number=");
+	scanf("%d",&m);
+	switch(m)
+	{
+	case 1:printf("january");
+		  break;
+	case 2:printf("february");
+		  break;
+	case 3:printf("march");
+		  break;
+	case 4:printf("april");
+		  break;
+	case 5:printf("may");
+		  break;
+	case 6:printf("june");
+		  break;
+	case 7:printf("july");
+		  break;
+	case 8:printf("august");
+		  break;
+	case 9:printf("september");
+		  break;
+	case 10:printf("october");
+		  break;
+	case 11:printf("november");
+		  break;
+	case 12:printf("december");
+		  break;
+	default:printf("invalid month.");
+		   break;
+	}
+}
+	 
